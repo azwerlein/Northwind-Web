da979a5db59c581502f10eebd1dc8b90d0b85533
azwerlein <azwerlein@my.wctc.edu>
2023-10-14 23:46:15 -0500

    Filter discounts

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,6 @@ namespace Northwind.Controllers
     {
         private DataContext _dataContext;
         public HomeController(DataContext db) => _dataContext = db;
-        public ActionResult Index() => View(_dataContext.Discounts);
+        public ActionResult Index() => View(_dataContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
     }
 }
