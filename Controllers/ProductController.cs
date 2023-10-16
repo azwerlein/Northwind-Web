2e9625f7d87e26cf52139aee88c411fb46adc9ba
azwerlein <azwerlein@my.wctc.edu>
2023-10-16 15:01:04 -0500

    Create discounts page

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,6 +7,7 @@ namespace Northwind.Controllers
         private DataContext _dataContext;
         public ProductController(DataContext db) => _dataContext = db;
         public ActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
+        public ActionResult Discounts() => View(_dataContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).OrderBy(d => d.EndTime));
         public ActionResult Index(int id) => View(_dataContext.Products.Where(p => p.CategoryId == id && !p.Discontinued).OrderBy(p => p.ProductName));
     }
 }
