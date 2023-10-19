f9c497aed997f562d74383d5d5f512aa1c3c81fe
azwerlein <azwerlein@my.wctc.edu>
2023-10-18 20:27:35 -0500

    Customer data migrations [CreateCustomer]

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -8,4 +8,5 @@ public class DataContext : DbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<Discount> Discounts { get; set; }
+    public DbSet<Customer> Customers { get; set; }
 }
